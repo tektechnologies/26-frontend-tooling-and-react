c97466d15b2991f7fffa8fdfcabb6246fb28a118
tektechnologies <info@tektechnologies.com>
2018-09-17 16:28:45 -0500

    add day 26 lab

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Learning Objectives
 
-* Students will be able to use `create-react-app` to start a React project
+*DONE - Students will be able to use `create-react-app` to start a React project
 * Students will be able to create and render react components to the DOM
 * Students will be able to add event listeners to react components
 * Students will be able to update react component state
@@ -20,7 +20,7 @@ Create the following component
 * Should contain the entire application's view and state
 * Should have a property on the state called content
 * Should create a view with the following display
-  * A heading with the title "Generate Cowsay Lorem"
+  * DONE = A heading with the title "Generate Cowsay Lorem"
   * A Button that displays "click me"
     * `onClick` the button should generate new content on the app state using cowsay and faker
   * A pre tag that displays the App state (content)
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let faker = require('faker');
+let cowsay = require('cowsay');
+
+//This is the State of our App
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        content: cowsay.say({
+        text: faker.lorem.word(),
+      })
+    }
+    this.updateState = this.updateState.bind(this);
+    this.newCowSay = this.newCowSay.bind(this);
+
+  }
+updateState(content){
+  this.setState({content});
+}
+  
+ newCowSay(){
+   this.updateState(cowsay.say({text: faker.lorem.word()}));
+ } 
+
+  
+//state
+  render() {
+    return (
+      <React.Fragment>
+       <section>
+          <h1 className="App-title">Generate Cowsay Lorem</h1>
+          <div className="container">
+                <div id="">
+                {this.state.content}
+                </div>
+             <button onClick={this.newCowSay}>Click Me</button>
+           </div>
+       
+        </section>
+      </React.Fragment>
+    );
+  }
+}//close class
+
+export default App;
